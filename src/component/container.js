7bc7767a3f24805789b352d37023d66cc1496022
cpplovelove <gsgslolo@naver.com>
2022-10-17 00:38:43 +0900

    refactor : 폴더 정리
    - component 안으로 화면구성요소 이동

--- src/component/container.js
+++ src/component/container.js
@@ -3,6 +3,27 @@ import Table from "../elements/table";
 export default function Container() {
   return (
     <div className="AppSetting">
+      <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>Saba</title>
+        <meta
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          name="viewport"
+        />
+        <link rel="stylesheet" href="dist/adminlte.min.css" />
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v6.0.0/css/all.css"
+        />
+      </head>
       <body class="hold-transition sidebar-mini">
         <div class="wrapper">
           <div class="content-wrapper">
