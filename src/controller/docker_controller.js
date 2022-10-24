4e6325d0d32a3f0631feb8823886c7f5411a9b7b
cpplovelove <gsgslolo@naver.com>
2022-10-24 07:17:47 +0000

    merge conflict

---
+++ src/controller/docker_controller.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+import $ from "jquery";
+import {SERVER_ADDRESS} from "../config/config";
+import { DockerService } from "../service/docker_service";
+
+export class DockerServiceController{
+    static container(){
+        const resJson = DockerService.container()
+        return resJson;
+    }
+
+    static start(container_id) {
+        const resJson = DockerService.start(container_id);
+        return resJson;
+    }
+
+    static stop(container_id){
+        const resJson = DockerService.stop(container_id);
+        return resJson;
+    }
+
+    static restart(container_id){
+        const resJson = DockerService.restart(container_id);
+        return resJson;
+    }
+
+    static remove(container_id){
+        const resJson = DockerService.remove(container_id);
+        return resJson;
+    }
+
+}
---
+++ src/service/docker_service.js
@@ -0,0 +1,104 @@
+
+/* eslint-disable */
+import $ from "jquery";
+import {SERVER_ADDRESS,token} from "../config/config";
+
+export class DockerService{
+    static container(){
+        const resJson ={
+            state_keys:
+            {
+                running: '<p class="text-white bg-success">실행</p>',
+                exited: '<p class="text-white bg-danger">종료</p>',
+                created: "생성",
+                restarting: "재시작",
+                paused: "일시정지",
+            }
+        }
+
+        return new Promise(function (resolve, reject) {
+            $.ajax({
+            method: "GET",
+            url: SERVER_ADDRESS + "/container/"+token,
+            contentType: "application/json",
+            success: function (data) {
+                resJson.container_data=data["containers"];
+                resolve(resJson);
+              },
+            error: function (request, status, error) {
+                console.log(request, status, error);
+                reject(error);
+            },
+            });
+        });
+    }
+
+
+    static start(container_id){
+        $.ajax({
+            method: "POST",
+            url: SERVER_ADDRESS + "/container/start"+token,
+            data: JSON.stringify({ container_id: container_id }),
+            contentType: "application/json",
+            success: function (data) {
+              console.log(data);
+              resolve(data);
+        },
+        error: function (request, status, error) {
+            console.log(request, status, error);
+            return false;
+        }
+        });
+
+    }
+
+    static stop(container_id) {
+      $.ajax({
+        method: "POST",
+        url: SERVER_ADDRESS + "/container/stop"+token,
+        data: JSON.stringify({ container_id: container_id }),
+        contentType: "application/json",
+        success: function (data) {
+          console.log(data);
+          resolve(data);
+        },
+        error: function (request, status, error) {
+          console.log(request, status, error);
+          return false;
+        }   
+       });
+  }
+
+
+  static restart(container_id) {
+    $.ajax({
+      method: "POST",
+      url: SERVER_ADDRESS + "/container/restart"+token,
+      data: JSON.stringify({ container_id: container_id }),
+      contentType: "application/json",
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (request, status, error) {
+        console.log(request, status, error);
+        return false;
+      }
+    });
+  }
+
+  static remove(container_id) {
+      $.ajax({
+        method: "POST",
+        url: SERVER_ADDRESS + "/container/remove"+token,
+        data: JSON.stringify({ container_id: container_id }),
+        contentType: "application/json",
+        success: function (data) {
+          console.log(data);
+        },
+        error: function (request, status, error) {
+          console.log(request, status, error);
+          return false;
+        }
+      });
+    }
+}
