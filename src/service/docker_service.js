6dc190288d670e1718d9f3acae1598574c291d0d
s2코딩천사선영이s2 <57889360+cpplovelove@users.noreply.github.com>
2022-10-26 01:00:23 +0900

    Revert "[8-BUF] 버튼 에러 수정"

--- src/service/docker_service.js
+++ src/service/docker_service.js
@@ -1,74 +1,77 @@
-/* eslint-disable */
 import $ from "jquery";
-import { SERVER_ADDRESS, token } from "../config/config";
+import {SERVER_ADDRESS,token} from "../config/config";
 
-export class DockerService {
-  static container() {
-    const resJson = {
-      state_keys: {
-        running: '<p class="text-white bg-success">실행</p>',
-        exited: '<p class="text-white bg-danger">종료</p>',
-        created: "생성",
-        restarting: "재시작",
-        paused: "일시정지",
-      },
-    };
+export class DockerService{
+    static container(){
+        const resJson ={
+            state_keys:
+            {
+                running: '<p class="text-white bg-success">실행</p>',
+                exited: '<p class="text-white bg-danger">종료</p>',
+                created: "생성",
+                restarting: "재시작",
+                paused: "일시정지",
+            }
+        }
 
-    return new Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
+            $.ajax({
+            method: "GET",
+            url: SERVER_ADDRESS + "/container/"+token,
+            contentType: "application/json",
+            success: function (data) {
+                resJson.container_data=data["containers"];
+                resolve(resJson);
+              },
+            error: function (request, status, error) {
+                console.log(request, status, error);
+                reject(error);
+            },
+            });
+        });
+    }
+
+
+    static start(container_id){
+        $.ajax({
+            method: "POST",
+            url: SERVER_ADDRESS + "/container/start"+token,
+            data: JSON.stringify({ container_id: container_id }),
+            contentType: "application/json",
+            success: function (data) {
+              console.log(data);
+              resolve(data);
+        },
+        error: function (request, status, error) {
+            console.log(request, status, error);
+            return false;
+        }
+        });
+
+    }
+
+    static stop(container_id) {
       $.ajax({
-        method: "GET",
-        url: SERVER_ADDRESS + "/container/" + token,
+        method: "POST",
+        url: SERVER_ADDRESS + "/container/stop"+token,
+        data: JSON.stringify({ container_id: container_id }),
         contentType: "application/json",
         success: function (data) {
-          resJson.container_data = data["containers"];
-          resolve(resJson);
+          console.log(data);
+          resolve(data);
         },
         error: function (request, status, error) {
           console.log(request, status, error);
-          reject(error);
-        },
-      });
-    });
+          return false;
+        }   
+       });
   }
 
-  static start(container_id) {
-    $.ajax({
-      method: "POST",
-      url: SERVER_ADDRESS + "/container/start" + token,
-      data: JSON.stringify({ container_id: container_id }),
-      contentType: "application/json",
-      success: function (data) {
-        console.log(data);
-        resolve(data);
-      },
-      error: function (request, status, error) {
-        console.log(request, status, error);
-        return false;
-      },
-    });
-  }
-
-  static stop(container_id) {
-    $.ajax({
-      method: "POST",
-      url: SERVER_ADDRESS + "/container/stop" + token,
-      data: JSON.stringify({ container_id: container_id }),
-      contentType: "application/json",
-      success: function (data) {
-        console.log(data);
-        resolve(data);
-      },
-      error: function (request, status, error) {
-        console.log(request, status, error);
-        return false;
-      },
-    });
-  }
 
   static restart(container_id) {
     $.ajax({
       method: "POST",
-      url: SERVER_ADDRESS + "/container/restart" + token,
+      url: SERVER_ADDRESS + "/container/restart"+token,
       data: JSON.stringify({ container_id: container_id }),
       contentType: "application/json",
       success: function (data) {
@@ -77,23 +80,23 @@ export class DockerService {
       error: function (request, status, error) {
         console.log(request, status, error);
         return false;
-      },
+      }
     });
   }
 
   static remove(container_id) {
-    $.ajax({
-      method: "POST",
-      url: SERVER_ADDRESS + "/container/remove" + token,
-      data: JSON.stringify({ container_id: container_id }),
-      contentType: "application/json",
-      success: function (data) {
-        console.log(data);
-      },
-      error: function (request, status, error) {
-        console.log(request, status, error);
-        return false;
-      },
-    });
-  }
+      $.ajax({
+        method: "POST",
+        url: SERVER_ADDRESS + "/container/remove"+token,
+        data: JSON.stringify({ container_id: container_id }),
+        contentType: "application/json",
+        success: function (data) {
+          console.log(data);
+        },
+        error: function (request, status, error) {
+          console.log(request, status, error);
+          return false;
+        }
+      });
+    }
 }
