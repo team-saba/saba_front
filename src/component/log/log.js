3f1dce40ede7cb911a65697e2f144b466e8bd078
cpplovelove <gsgslolo@naver.com>
2022-10-27 05:36:20 +0000

    feat: 라우팅 경로 추가
    - vulnerability/ log 경로 추가

---
+++ src/component/log/log.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+
+export default function Log() {
+  return (
+    <div className="AppSetting">
+      <containerHeader></containerHeader>
+      <body class="hold-transition sidebar-mini">
+        <div class="wrapper">
+          <div class="content-wrapper">
+            <div class="content-header">
+              <div class="container-fluid">
+                <div class="row mb-2">
+                  <div class="col-sm-6">
+                    <h1 class="m-0">Log</h1>
+                  </div>
+                  <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                      <li class="breadcrumb-item">
+                        <a href="#">Log</a>
+                      </li>
+                    </ol>
+                  </div>
+                </div>
+                <br />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <script src="plugins/jquery/jquery.min.js"></script>
+        <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="dist/js/adminlte.js"></script>
+
+        <script src="plugins/chart.js/Chart.min.js"></script>
+        <script src="dist/js/demo.js"></script>
+        <script src="dist/js/pages/dashboard3.js"></script>
+      </body>
+    </div>
+  );
+}
---
+++ src/component/vulner/vulner.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+export default function Vulnerability() {
+  return (
+    <div className="AppSetting">
+      <containerHeader></containerHeader>
+      <body class="hold-transition sidebar-mini">
+        <div class="wrapper">
+          <div class="content-wrapper">
+            <div class="content-header">
+              <div class="container-fluid">
+                <div class="row mb-2">
+                  <div class="col-sm-6">
+                    <h1 class="m-0">Vulnerability</h1>
+                  </div>
+                  <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                      <li class="breadcrumb-item">
+                        <a href="#">Vulnerability</a>
+                      </li>
+                    </ol>
+                  </div>
+                </div>
+                <br />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <script src="plugins/jquery/jquery.min.js"></script>
+        <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="dist/js/adminlte.js"></script>
+
+        <script src="plugins/chart.js/Chart.min.js"></script>
+        <script src="dist/js/demo.js"></script>
+        <script src="dist/js/pages/dashboard3.js"></script>
+      </body>
+    </div>
+  );
+}
