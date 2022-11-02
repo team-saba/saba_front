c36de1545bb5c77cd356c18da39bf5cad56ca435
cpplovelove <gsgslolo@naver.com>
2022-11-02 15:52:41 +0000

    refactor: docker 서비스 파일 수정
    - axios 적용

--- src/service/docker_service.js
+++ src/service/docker_service.js
@@ -6,88 +6,47 @@ import instance from "./axiosConfig";
 
 export class DockerService {
   static container() {
-    const resJson = {
-      state_keys: {
-        running: '<p class="text-white bg-success">실행</p>',
-        exited: '<p class="text-white bg-danger">종료</p>',
-        created: "생성",
-        restarting: "재시작",
-        paused: "일시정지",
-      },
-    };
     return new Promise((resolve, reject) => {
         instance.get("/container" + token).then((res) => {
-          console.log(res.data);
+          if(!res.data) reject(new Error(500))
           resolve(res.data);
         });
     })
   }
 
   static start(container_id) {
-    $.ajax({
-      method: "POST",
-      url: SERVER_ADDRESS + "/container/start" + token,
-      data: JSON.stringify({ container_id: container_id }),
-      contentType: "application/json",
-      success: function (data) {
-        console.log(data);
-        // eslint-disable-next-line no-undef
-        resolve(data);
-      },
-      error: function (request, status, error) {
-        console.log(request, status, error);
-        return false;
-      },
-    });
+    return new Promise((resolve, reject) => {
+        instance.post("/container/start" + token,{container_id}).then((res) => {
+          if(!res.data) reject(new Error(500))
+          resolve(res.data);
+        });
+    })
   }
 
   static stop(container_id) {
-    $.ajax({
-      method: "POST",
-      url: SERVER_ADDRESS + "/container/stop" + token,
-      data: JSON.stringify({ container_id: container_id }),
-      contentType: "application/json",
-      success: function (data) {
-        console.log(data);
-        // eslint-disable-next-line no-undef
-        resolve(data);
-      },
-      error: function (request, status, error) {
-        console.log(request, status, error);
-        return false;
-      },
-    });
+    return new Promise((resolve, reject) => {
+        instance.post("/container/stop" + token,{container_id}).then((res) => {
+          if(!res.data) reject(new Error(500))
+          resolve(res.data);
+        });
+    })
   }
 
   static restart(container_id) {
-    $.ajax({
-      method: "POST",
-      url: SERVER_ADDRESS + "/container/restart" + token,
-      data: JSON.stringify({ container_id: container_id }),
-      contentType: "application/json",
-      success: function (data) {
-        console.log(data);
-      },
-      error: function (request, status, error) {
-        console.log(request, status, error);
-        return false;
-      },
-    });
+    return new Promise((resolve, reject) => {
+        instance.post("/container/restart" + token,{container_id}).then((res) => {
+          if(!res.data) reject(new Error(500))
+          resolve(res.data);
+        });
+    })
   }
 
   static remove(container_id) {
-    $.ajax({
-      method: "POST",
-      url: SERVER_ADDRESS + "/container/remove" + token,
-      data: JSON.stringify({ container_id: container_id }),
-      contentType: "application/json",
-      success: function (data) {
-        console.log(data);
-      },
-      error: function (request, status, error) {
-        console.log(request, status, error);
-        return false;
-      },
-    });
+    return new Promise((resolve, reject) => {
+        instance.post("/container/remove" + token,{container_id}).then((res) => {
+          if(!res.data) reject(new Error(500))
+          resolve(res.data);
+        });
+    })
   }
 }
