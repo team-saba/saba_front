6e826ceb4744b48250a3932b481e60d931d9bae2
cpplovelove <57889360+cpplovelove@users.noreply.github.com>
2022-11-05 01:30:34 +0900

    bug : 경로 수정

--- src/service/docker_service.js
+++ src/service/docker_service.js
@@ -5,46 +5,52 @@ import instance from "./axiosConfig";
 export class DockerService {
   static container() {
     return new Promise((resolve, reject) => {
-        instance.get("/container" + token).then((res) => {
-          if(!res.data) reject(new Error(500))
-          resolve(res.data);
-        });
-    })
+      instance.get("/container/" + token).then((res) => {
+        if (!res.data) reject(new Error(500));
+        resolve(res.data);
+      });
+    });
   }
 
   static start(container_id) {
     return new Promise((resolve, reject) => {
-        instance.post("/container/start" + token,{container_id}).then((res) => {
-          if(!res.data) reject(new Error(500))
+      instance
+        .post("/container/start" + token, { container_id })
+        .then((res) => {
+          if (!res.data) reject(new Error(500));
           resolve(res.data);
         });
-    })
+    });
   }
 
   static stop(container_id) {
     return new Promise((resolve, reject) => {
-        instance.post("/container/stop" + token,{container_id}).then((res) => {
-          if(!res.data) reject(new Error(500))
-          resolve(res.data);
-        });
-    })
+      instance.post("/container/stop" + token, { container_id }).then((res) => {
+        if (!res.data) reject(new Error(500));
+        resolve(res.data);
+      });
+    });
   }
 
   static restart(container_id) {
     return new Promise((resolve, reject) => {
-        instance.post("/container/restart" + token,{container_id}).then((res) => {
-          if(!res.data) reject(new Error(500))
+      instance
+        .post("/container/restart" + token, { container_id })
+        .then((res) => {
+          if (!res.data) reject(new Error(500));
           resolve(res.data);
         });
-    })
+    });
   }
 
   static remove(container_id) {
     return new Promise((resolve, reject) => {
-        instance.post("/container/remove" + token,{container_id}).then((res) => {
-          if(!res.data) reject(new Error(500))
+      instance
+        .post("/container/remove" + token, { container_id })
+        .then((res) => {
+          if (!res.data) reject(new Error(500));
           resolve(res.data);
         });
-    })
+    });
   }
 }
