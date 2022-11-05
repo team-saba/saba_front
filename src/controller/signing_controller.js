686a6a2c63b349de6d8276af3f7356f00f61d385
cpplovelove <57889360+cpplovelove@users.noreply.github.com>
2022-11-06 03:37:54 +0900

    docker login 관련 API 생성

--- src/controller/signing_controller.js
+++ src/controller/signing_controller.js
@@ -1,4 +1,35 @@
 import { SigningService } from "../service/signing_service";
 
 export class SigningServiceController {
+  static async login(id, pw) {
+    id = id.trim();
+    pw = pw.trim();
+    const resJson = await SigningService.login(id, pw);
+    console.log(resJson);
+    return resJson;
+  }
+
+  static logout() {
+    alert("현재 InternalServerError 뜸");
+    // const resJson = SigningService.logout();
+    // return resJson;
+  }
+
+  static async checkLogin() {
+    const resJson = await SigningService.checkLogin();
+    console.log(resJson.ID);
+    if (resJson.ID) {
+      const loggedId = resJson.ID;
+      alert(loggedId + "님이 로그인 중입니다.");
+    } else {
+      alert("로그인되지 않음");
+    }
+    return resJson;
+  }
+
+  static checkLoginId(id) {
+    alert("현재 InternalServerError 뜸");
+    // const resJson = SigningService.checkLoginId(id);
+    // return resJson;
+  }
 }
--- src/service/signing_service.js
+++ src/service/signing_service.js
@@ -1,6 +1,43 @@
 /* eslint-disable */
-import { SERVER_ADDRESS, token } from "../config/config";
 import instance from "./axiosConfig";
 
 export class SigningService {
+  static login(id, pw) {
+    return new Promise((resolve, reject) => {
+      instance
+        .post("/image/docker_login", null, { params: { id: id, pw: pw } })
+        .then((res) => {
+          if (!res.data) reject(new Error(500));
+          resolve(res.data);
+        });
+    });
+  }
+
+  static logout() {
+    return new Promise((resolve, reject) => {
+      instance.post("/image/docker_logout").then((res) => {
+        if (!res.data) reject(new Error(500));
+        resolve(res.data);
+      });
+    });
+  }
+
+  static checkLogin() {
+    return new Promise((resolve, reject) => {
+      instance.post("/image/docker_login_check").then((res) => {
+        if (!res.data) reject(new Error(500));
+        console.log(res.data);
+        resolve(res.data);
+      });
+    });
+  }
+
+  static checkLoginId(id) {
+    return new Promise((resolve, reject) => {
+      instance.post("/image/docker_login_id_check").then((res) => {
+        if (!res.data) reject(new Error(500));
+        resolve(res.data);
+      });
+    });
+  }
 }
