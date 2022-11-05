519bc459ae64e7979bc0a7e9025c598f2499c98c
Seungwook Lee <swlee@daegu.ac.kr>
2022-11-05 19:12:42 +0900

    Seungwook feature (#25)
    * 이미지 목록 서버 연동
    
    * to do memo

--- src/service/docker_service.js
+++ src/service/docker_service.js
@@ -1,50 +1,75 @@
 /* eslint-disable */
-import { SERVER_ADDRESS, token } from "../config/config";
+import { token } from "../config/config";
 import instance from "./axiosConfig";
 
 export class DockerService {
   static container() {
     return new Promise((resolve, reject) => {
-        instance.get("/container" + token).then((res) => {
-          if(!res.data) reject(new Error(500))
-          resolve(res.data);
-        });
-    })
+      instance.get("/container" + token).then((res) => {
+        if (!res.data) reject(new Error(500));
+        resolve(res.data);
+      });
+    });
   }
 
   static start(container_id) {
     return new Promise((resolve, reject) => {
-        instance.post("/container/start" + token,{container_id}).then((res) => {
-          if(!res.data) reject(new Error(500))
+      instance
+        .post("/container/start" + token, { container_id })
+        .then((res) => {
+          if (!res.data) reject(new Error(500));
           resolve(res.data);
         });
-    })
+    });
   }
 
   static stop(container_id) {
     return new Promise((resolve, reject) => {
-        instance.post("/container/stop" + token,{container_id}).then((res) => {
-          if(!res.data) reject(new Error(500))
-          resolve(res.data);
-        });
-    })
+      instance.post("/container/stop" + token, { container_id }).then((res) => {
+        if (!res.data) reject(new Error(500));
+        resolve(res.data);
+      });
+    });
   }
 
   static restart(container_id) {
     return new Promise((resolve, reject) => {
-        instance.post("/container/restart" + token,{container_id}).then((res) => {
-          if(!res.data) reject(new Error(500))
+      instance
+        .post("/container/restart" + token, { container_id })
+        .then((res) => {
+          if (!res.data) reject(new Error(500));
           resolve(res.data);
         });
-    })
+    });
   }
 
   static remove(container_id) {
     return new Promise((resolve, reject) => {
-        instance.post("/container/remove" + token,{container_id}).then((res) => {
-          if(!res.data) reject(new Error(500))
+      instance
+        .post("/container/remove" + token, { container_id })
+        .then((res) => {
+          if (!res.data) reject(new Error(500));
           resolve(res.data);
         });
-    })
+    });
+  }
+
+  //  image
+  static image() {
+    return new Promise((resolve, reject) => {
+      instance.get("/image/").then((res) => {
+        if (!res.data) reject(new Error(500));
+        resolve(res.data);
+      });
+    });
+  }
+
+  static scan(image_id) {
+    return new Promise((resolve, reject) => {
+      instance.post("/image/scan", { image_id }).then((res) => {
+        if (!res.data) reject(new Error(500));
+        resolve(res.data);
+      });
+    });
   }
 }
